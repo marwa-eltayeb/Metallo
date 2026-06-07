2d919c5ab5457be36da37db6eb6d171416081c46
Marwa Eltayeb <marwa_eltayeb@yahoo.com>
2026-06-07 17:22:08 +0200

    Create Home Screen

---
+++ lib/core/presentation/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:metallo/core/constants/app_colors.dart';
+import 'package:metallo/core/constants/app_strings.dart';
+import 'package:metallo/core/widgets/custom_button.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CustomBotton(text: AppStrings.gold, onPressed: (){}, color: AppColors.goldColor),
+          SizedBox(height: 16),
+          CustomBotton(text: AppStrings.silver, onPressed: (){}, color: AppColors.silverColor)
+        ],
+      ),
+    );
+  }
+}
